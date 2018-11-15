3861c76a1a7af37b2d1e892ca3c47894e35e3f1e
Felipe Augusto Gonçalves Basilio <43045359+FeehGb@users.noreply.github.com>
2018-11-15 01:59:46 -0200

    Refatorando o codigo e adicionando novas funcionalidades

--- gn_modal.js
+++ gn_modal.js
@@ -2,77 +2,146 @@ class Gn_modal{
     
     constructor(){
         
+        /**
+         * atributos para manipulacao do modal
+         */
         this.modal      = document.getElementById('gn_modal');
-        this.title      = this.modal.querySelector('.title-box');
-        this.message    = this.modal.querySelector('.desc-box');
-        this.controller = this.modal.querySelector('#box-controller')
+        this.title      = document.getElementById('title-box');
+        this.message    = document.getElementById('desc-box');
+        this.controller = document.getElementById('box-controller')
+        this.mainBox    = document.getElementById('main-box')
         
         /**
          * Set defaults values to de modal box
          */
         this.default = { 
-            title       :'Default title',
-            message     :'Descricao da caixa de dialogo',
-            type        :'info',
-            buttons     :{
+            title      :'type atitle',
+            message    :'type a description',
+            type       :'info',
+            buttons    :{
                 cancel  : ()=>{ this.dismiss();}
-            }
+            },
+            addClass    : '',
         };
+        
+        this.initPrototype();
     }
-    
+    /**
+     * Funcao que cria todos os elementos e atributos do modal
+     * 
+     * @param {object} value objeto com novos valores a serem definidos no modal
+     */
     create(value){
+        // limpa tudo antes de receber os novos valores
+        this.removeAllChild(this.title,this.message,this.controller);
         
-        this.defaultSetting =  this.settings(value);
-        this.modal.classList.add(`${this.defaultSetting.type}`);
+        // define as configuracoes padrao
+        this.defaultSetting =   this.settings(value);
         
-        //Criando titulo
+        // adiciona a classe ao main-box
+        this.mainBox.classList  .add(`${this.defaultSetting.type}`);
+        
+        // Checa se a propriedade addclass foi declarar
+        this.defaultSetting.addClass != ''?
+            this.modal.classList.add(`${this.defaultSetting.addClass}`) 
+        :'' ;
+    
+        // Criando titulo
         this.title.append( this.createNewElement('h3',`${this.defaultSetting.title}`));  
         
-        //Crinado descricao
+        // Crinado descricao
         this.message.append(this.createNewElement('p',`${this.defaultSetting.message}`));
         
         //Criando os botoes
         let button = [];
         for (button  in this.defaultSetting.buttons){
-            
             this.controller.append( this.createNewElement('button',`${button}`)) 
-            
-            
         }  
         
+        this.modal.show();
     }
     
-    createNewElement( elm , value ){
-        
+    
+    /**
+     *  Funcao para criar tag html para o DOM
+     * 
+     * @param {String} elm          // Tag html que deseja criar
+     * @param {String} innerText    // texto que ira dentro da nova tag
+     * @returns {newElement}        // Novo elemento criado
+     */
+    createNewElement( elm , innerText ){ 
         let newElement = document.createElement(`${elm}`);
-        newElement.innerHTML = `${value}`;
+        newElement.innerHTML = `${innerText}`;
         
         if(elm === 'button'){
-            newElement.dataset.fn = `${value}`;
+            newElement.dataset.fn = `${innerText}`;
             this.addEventsButtons(newElement);
         }
-        
         return newElement;
-        
     }
     
-    addEventsButtons(button){
-        
-        button.addEventListener('click',this.defaultSetting.buttons[button.dataset.fn]);
-        
+    /**
+     *  Funcao para adicionar eventos de click nos botoes
+     * 
+     * @param {Element} button //Elemento button html
+     */
+    addEventsButtons(button){  
+        button.addEventListener('click',this.defaultSetting.buttons[button.dataset.fn]);  
     }
-    
+    /**
+     * Funcao para atribuir valores entre as padrao
+     * e as novas configuracoes
+     * 
+     * @param {object} newSettings  //objeto com as novas configuracoes
+     * @returns {Object.assign}     // Objeto de atribuicao
+     * 
+     */
     settings(newSettings){
-        
-        return Object.assign({}, this.default, newSettings );
+    
+        if(newSettings instanceof Object){
+            return Object.assign({}, this.default, newSettings );
+        }else{
+            console.error('Valores padroes atribuido a variavel, pois parametro passado nao era do tipo Object')
+            return this.default;
+        }
+    }
+    
+    /**
+     *  Funcao que dispensa e limpa o modal
+     */
+    dismiss(){
+
+        this.modal.hide();
+        this.removeAllChild(this.title,this.message,this.controller);
         
     }
     
-    dismiss(){
+    removeAllChild(...params){
         
-        this.title.innerHTML = '';
-        this.message.innerHTML = ''; 
-        this.controller.innerHTML = '';
+        params.forEach((param)=>{
+            while (param.firstChild) param.removeChild(param.firstChild);
+        }); 
+    }
+    
+    
+    initPrototype(){
+        
+        Element.prototype.show = function(){
+            this.style.display = 'flex';
+        }
+        
+        
+        Element.prototype.hide = function(){
+            this.style.display = 'none';
+        }
+        
         
     }
+    
+    
 }
+
+
+
+
+
